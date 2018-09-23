6ae2d63f89db1ef4d5bc6320c0128768ba424c14
Sitati Kituyi <sitati.kituyi@googlemail.com>
2018-09-23 12:26:36 +0300

    Add Velocity templating

--- spark/devcraftml/src/main/kotlin/devcraftml/SparkApp.kt
+++ spark/devcraftml/src/main/kotlin/devcraftml/SparkApp.kt
@@ -1,7 +1,10 @@
 @file:JvmName("SparkApp")
 package devcraftml
 
+import spark.ModelAndView
 import spark.Spark.*
+import spark.template.velocity.VelocityTemplateEngine
+import java.util.*
 
 fun main(args: Array<String>) {
     port(8080)
@@ -13,6 +16,7 @@ fun main(args: Array<String>) {
     })
 
     get("/image", { req, res ->
-        latestImageData
+        val model = HashMap<String, Any?>()
+        VelocityTemplateEngine().render(ModelAndView(model, "image.vm"))
     })
 }
